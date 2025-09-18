c88080d4e6bc5280151b44f8e1ceeb4b22f4f72f
Bianca Mustelier <bianca.mustelier@g.austincc.edu>
2025-09-18 00:52:57 +0000

    tests_in_proc_out edit

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -1,2 +1,8 @@
-import decisions
+from decisions import is_even
+
+def main ():
+    print (is even(2))
+    print (is even(3))
+    if_name__=="__main__":
+        main()
 
--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -6,4 +6,8 @@ class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
+        def test_is_even(self):
+            self.assertTrue(is_even) (2)
+            self.assertFalse(is_even) (3)
+    
 
