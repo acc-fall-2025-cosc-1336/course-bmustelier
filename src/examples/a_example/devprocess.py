223c8314f12c5d95f432efb64cc539ea911e4dd2
Bianca Mustelier <bianca.mustelier@g.austincc.edu>
2025-09-21 01:56:13 +0000

    changes a

--- src/examples/a_example/devprocess.py
+++ src/examples/a_example/devprocess.py
@@ -1,4 +1,5 @@
 def hello_world(name):
    print("Hello, " + name + "!")
 
-    
+def echo_value(value):
+         return value
--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -1,7 +1,8 @@
-from devprocess import hello_world
+from devprocess import hello_world, echo_value
 
 def main():
     hello_world("Bianca")
+    print(echo_value(5))
 
 if __name__ == "__main__":
     main()
