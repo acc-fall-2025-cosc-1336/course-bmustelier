b0ba04ed753d7b99f963c8c200f4eb5674636749
Bianca Mustelier <bianca.mustelier@g.austincc.edu>
2025-09-21 03:21:55 +0000

    receipts homework

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,4 @@
 import unittest
-'''
-the file in /tests/homework/b_in_proc_out/tests_in_proc_out
-has the test functions
-'''
 from tests.homework.b_in_proc_out import tests_in_proc_out
 
 suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
