d331163232ea07c8a0baf924aa93de96c187b6c5
Bianca Mustelier <bianca.mustelier@g.austincc.edu>
2025-09-22 11:23:39 +0000

    output update

--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,6 +1,16 @@
 def get_sales_tax_amount(meal_amount):
+    """
+    Calculate the sales tax amount for a given meal amount.
+
+    Parameters:
+        meal_amount (float): The cost of the meal.
+
+    Returns:
+        float: The calculated sales tax amount.
+    """
     tax_rate = 0.0675
     return meal_amount * tax_rate
+    return meal_amount * TAX_RATE
 
 def get_tip_amount(meal_amount, tip_rate):
     return meal_amount * tip_rate
