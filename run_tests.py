a223f06f7c50d8f2565b8f09e89977b21c5e786e
Bianca Mustelier <bianca.mustelier@g.austincc.edu>
2025-09-28 01:12:44 +0000

    commit test

--- run_tests.py
+++ run_tests.py
@@ -3,3 +3,7 @@ import unittest
 the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
+from tests.homework.c_decisions import tests_decisions as tests_decisions
+
+suite = unittest.TestLoader().loadTestsFromModule(tests_decisions)
+unittest.TextTestRunner(verbosity=2).run(suite)
