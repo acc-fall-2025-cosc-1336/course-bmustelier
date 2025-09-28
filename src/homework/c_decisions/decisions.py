5e1b8fe3d078945cbd2354a713a6844bc7351156
Bianca Mustelier <bianca.mustelier@g.austincc.edu>
2025-09-28 02:49:48 +0000

    hw03 commits

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -10,4 +10,29 @@ def get_letter_grade(score):
         return 'D'
     else:
         return 'F'
-    
+
+# Faculty rating functions
+
+def get_options_ratio(option, total):
+    """Return the ratio of option to total."""
+    if total == 0:
+        return 0
+    return option / total
+
+
+def get_faculty_rating(ratio):
+    """Return the faculty rating based on the ratio."""
+    if not (0 <= ratio < 1):
+        return "Invalid Ratio"
+    if 0.9 <= ratio < 1:
+        return "Excellent"
+    elif ratio > 0.8:
+        return "Very Good"
+    elif ratio > 0.7:
+        return "Good"
+    elif ratio > 0.6:
+        return "Needs Improvement"
+    elif 0 <= ratio <= 0.59:
+        return "Unacceptable"
+
+
