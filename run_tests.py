aeefe9f1da59a44bb62584c4f6d339e82dc4e96c
Bianca Mustelier <bianca.mustelier@g.austincc.edu>
2025-11-02 02:22:06 +0000

    dna

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from tests.homework.e_functions import tests_functions as tests_functions
+from tests.homework.h_strings import tests_strings as tests_strings
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_functions)
+suite = unittest.TestLoader().loadTestsFromModule(tests_strings)
 unittest.TextTestRunner(verbosity=2).run(suite)
