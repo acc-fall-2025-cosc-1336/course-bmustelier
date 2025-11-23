cb23ce5752aa7b19bdccf3f01abd643331cc68d2
Bianca Mustelier <bianca.mustelier@g.austincc.edu>
2025-11-23 18:52:51 +0000

    hw09 dictionary

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,13 @@
-#
+#def add_inventory(widgets, widget_name, quantity):
+	if widget_name in widgets:
+		widgets[widget_name] = widgets[widget_name] + quantity
+	else:
+		widgets[widget_name] = quantity
+	return None
+
+
+def remove_inventory_widget(widgets, widget_name):
+	if widget_name in widgets:
+		del widgets[widget_name]
+		return 'Record deleted'
+	return 'Item not found'
