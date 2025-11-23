368113a8cc225672ebfe9c1fc0d08804d87ea14a
Bianca Mustelier <bianca.mustelier@g.austincc.edu>
2025-11-23 18:56:36 +0000

    hw09 test

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,24 @@
-#
+#import unittest
+from src.homework.i_dictionaries_sets.dictionary import add_inventory
+
+
+class Test_Config(unittest.TestCase):
+
+	def test_add_inventory(self):
+		inventory = {}
+		# add Widget1 with quantity 10 -> should be 10
+		add_inventory(inventory, 'Widget1', 10)
+		self.assertIn('Widget1', inventory)
+		self.assertEqual(inventory['Widget1'], 10)
+
+		# add Widget1 with quantity 25 -> total should be 35
+		add_inventory(inventory, 'Widget1', 25)
+		self.assertEqual(inventory['Widget1'], 35)
+
+		# add Widget1 with quantity -10 -> total should be 25
+		add_inventory(inventory, 'Widget1', -10)
+		self.assertEqual(inventory['Widget1'], 25)
+
+
+if __name__ == '__main__':
+	unittest.main()
